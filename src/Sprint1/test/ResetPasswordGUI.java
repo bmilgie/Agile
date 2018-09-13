cc7292a489ff7a22816af3dd12a844794cba37a4
angelsanabria <angelsanabria@u.boisestate.edu>
2018-09-13 10:20:55 -0600

    Pushing Bridgette's tests for reset password

---
+++ src/Sprint1/test/ResetPasswordGUI.java
@@ -0,0 +1,47 @@
+package Tests;
+import java.awt.Dimension;
+
+import javax.swing.*;
+import org.junit.*;
+import Login.*;
+
+public class ResetPasswordGUI {
+	private MainPanel mainPanel;
+	private Login loginPanel;
+	private Profile profile;
+	private Register register;
+	private Driver driver;
+	private ResetPassword resetPanel;
+	
+	@Before
+	public void setUp()
+	{
+		mainPanel = new MainPanel();
+		register = new Register(mainPanel);
+		loginPanel = new Login(mainPanel);
+		profile = new Profile(mainPanel);
+		resetPanel = new ResetPassword(mainPanel);
+		
+		mainPanel.constructPanels(loginPanel, resetPanel, register, profile);
+		driver = new Driver();
+	}
+	
+	@After
+	public void done()
+	{
+		
+	}
+	
+	@Test
+	public void loginGui()
+	{
+		driver.createAndShowGUI();
+		Login.reset.doClick();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/Sprint1/test/ResetPasswordWithEmailPass.java
@@ -0,0 +1,60 @@
+package Tests;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import org.junit.*;
+
+import Login.*;
+
+public class ResetPasswordWithEmailPass {
+	private MainPanel mainPanel;
+	private Login loginPanel;
+	private Profile profile;
+	private Register register;
+	private ResetPassword resetPanel;
+
+	
+	@Before
+	public void setUp()
+	{
+		mainPanel = new MainPanel();
+		register = new Register(mainPanel);
+		loginPanel = new Login(mainPanel);
+		profile = new Profile(mainPanel);
+		resetPanel = new ResetPassword(mainPanel);
+		
+		mainPanel.constructPanels(loginPanel, resetPanel, register, profile);
+	}
+	@After
+	public void done()
+	{
+		
+	}
+	public void sleepFor(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	@Test
+	public void runTest()
+	{
+		Driver.createAndShowGUI();
+		Login.reset.doClick();
+		ResetPassword.emailText.setText("bmilgie@me.com");
+		ResetPassword.sqText.setText("anakin");
+		ResetPassword.submitButton.doClick();
+		String password=  ResetPassword.passwordLabel.getText();
+			   password = password.replace("Your new password is : ", "");
+	    ResetPassword.backButton.doClick();
+	    Login.username.setText("bridgette");
+		Login.password.setText(password);
+		Login.submit.doClick();
+		
+	}
+	
+}
